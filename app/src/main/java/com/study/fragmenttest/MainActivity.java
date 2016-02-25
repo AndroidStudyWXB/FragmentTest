1493a7f508aee22ff67c453e59ded9b252bcec5d
WXB506 <1922686887@qq.com>
2016-02-25 22:29:34 +0800

    dynamic replace fragment

--- app/src/main/java/com/study/fragmenttest/MainActivity.java
+++ app/src/main/java/com/study/fragmenttest/MainActivity.java
@@ -1,13 +1,35 @@
 package com.study.fragmenttest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View.OnClickListener;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                AnotherRightFragment fragment   = new AnotherRightFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.right_layout, fragment);
+                transaction.commit();
+                break;
+            default:
+                break;
+        }
     }
 }
